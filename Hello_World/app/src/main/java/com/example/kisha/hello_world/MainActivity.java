363a75d7efc6a0f9c444c6f1c1efb3f9ac4af892
Kishan_KK <kishankalburgi22@gmail.com>
2017-07-01 20:00:13 +0530

    Adding files

---
+++ Hello_World/app/src/main/java/com/example/kisha/hello_world/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kisha.hello_world;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
